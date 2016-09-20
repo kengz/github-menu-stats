672082f6d65e30dbab72978cad5517540a700fb5
kengz <kengzwl@gmail.com>
2016-09-19 23:39:28 -0400

    init app with yo, config devtron debugger

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+const { app, BrowserWindow } = require('electron');
+
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+
+// prevent window being garbage collected
+let mainWindow;
+
+function onClosed() {
+  // dereference the window
+  // for multiple windows store them in an array
+  mainWindow = null;
+}
+
+function createMainWindow() {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 400
+  });
+
+  win.loadURL(`file://${__dirname}/index.html`);
+  win.on('closed', onClosed);
+
+  return win;
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (!mainWindow) {
+    mainWindow = createMainWindow();
+  }
+});
+
+app.on('ready', () => {
+  mainWindow = createMainWindow();
+});
